e1e2bd5f10c30b1540456af73ae46825daa877a6
Duubemmm <dubykol@gmail.com>
2025-03-17 13:42:01 +0100

    refactor: update styles and main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ function calculateRepayment(event) {
         <h2 id="display-result-heading">Your results</h2>
         <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click 'calculate repayments' again.</p>
         <div class="monthlypayment-div">
-            <p>Your monthly Interest</p>
+            <p>Your monthly interest</p>
             <h1>$${monthlyInterest.toFixed(3)}</h1>
             <p>Total you'll repay over the term</p>
             <h2 id="display-monthly-repayment">$${totalInterest.toFixed(3)}</h2>
